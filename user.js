b4d6167515b8f2d5d9de9bc6a8b494a0634e99ed
Basel Arafat <baselarafat@Basels-MacBook-Pro.local>
2018-03-03 14:06:50 -0500

    laala

--- user.js
+++ user.js
@@ -1,8 +1,6 @@
 var myMap = new Map();
 
 var keyString = 'a string',
-    keyObj = {},
-    keyFunc = function() {};
 
 // setting the values
 myMap.set(keyString, "value associated with 'a string'");
@@ -18,5 +16,3 @@ myMap.get(keyFunc);      // "value associated with keyFunc"
 
 myMap.get('a string');   // "value associated with 'a string'"
                          // because keyString === 'a string'
-myMap.get({});           // undefined, because keyObj !== {}
-myMap.get(function() {}) // undefined, because keyFunc !== function () {}
