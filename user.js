fc13d554de0f42aaa482bab7db932ecb1b939aa5
Basel Arafat <baselarafat@Basels-MacBook-Pro.local>
2018-03-03 14:07:17 -0500

    I have no idea

--- user.js
+++ user.js
@@ -1,18 +1,17 @@
 var myMap = new Map();
 
 var keyString = 'a string',
+  
 
 // setting the values
 myMap.set(keyString, "value associated with 'a string'");
-myMap.set(keyObj, 'value associated with keyObj');
-myMap.set(keyFunc, 'value associated with keyFunc');
+
 
 myMap.size; // 3
 
 // getting the values
 myMap.get(keyString);    // "value associated with 'a string'"
-myMap.get(keyObj);       // "value associated with keyObj"
-myMap.get(keyFunc);      // "value associated with keyFunc"
+
 
 myMap.get('a string');   // "value associated with 'a string'"
                          // because keyString === 'a string'
