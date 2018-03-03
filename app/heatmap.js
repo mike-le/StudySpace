ce43580f580a1b2948395a645448f1131440075a
wpflueger <will.pflueger@gmail.com>
2018-03-03 17:15:26 -0500

    richmond

--- app/heatmap.js
+++ app/heatmap.js
@@ -1,27 +1,13 @@
 /* Data points defined as an array of LatLng objects */
 var heatmapData = [
-  new google.maps.LatLng(37.782, -122.447),
-  new google.maps.LatLng(37.782, -122.445),
-  new google.maps.LatLng(37.782, -122.443),
-  new google.maps.LatLng(37.782, -122.441),
-  new google.maps.LatLng(37.782, -122.439),
-  new google.maps.LatLng(37.782, -122.437),
-  new google.maps.LatLng(37.782, -122.435),
-  new google.maps.LatLng(37.785, -122.447),
-  new google.maps.LatLng(37.785, -122.445),
-  new google.maps.LatLng(37.785, -122.443),
-  new google.maps.LatLng(37.785, -122.441),
-  new google.maps.LatLng(37.785, -122.439),
-  new google.maps.LatLng(37.785, -122.437),
-  new google.maps.LatLng(37.785, -122.435)
 ];
 
-var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+var Richmond = new google.maps.LatLng(37.5843, -77.5734);
 
 map = new google.maps.Map(document.getElementById('map'), {
   center: sanFrancisco,
   zoom: 13,
-  mapTypeId: 'satellite'
+  mapTypeId: 'roadmap'
 });
 
 var heatmap = new google.maps.visualization.HeatmapLayer({
